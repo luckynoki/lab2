c3e2c99a6aec9476772c0bb68b11cabadd8ffd52
luckynokian <luckynokian@gmail.com>
2023-12-07 04:55:39 +0000

    Вроде бы рабочая версия скрипта, даже ALLOWED_HOSTS учтена...

---
+++ lab2setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DISALLOWED_IP="$(ip a s ens33 | grep inet | head --lines=1 | awk '{print $2}' | cut -d'/' -f1 | tr -d '\n')"
+
+sudo apt install -y python3 python3-pip
+pip install Django==4.0
+pip install Pillow
+git clone https://github.com/PacktPublishing/Django-4-for-the-Impatient
+cp -R Django-4-for-the-Impatient/Chapter11/moviereviewsproject/ moviereviewsproject
+cd moviereviewsproject
+sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['$DISALLOWED_IP'\]/g' ./moviereviews/settings.py
+python3 manage.py runserver 0.0.0.0:8000
